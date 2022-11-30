cd62009fd3b795403d90621ac5baa5c6f113a3a8
akm <akm@FATIMA>
2022-11-30 22:50:34 +0100

    Add project files.

---
+++ FormsApp1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace FormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAjout = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupp = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Nom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fORMDataSet = new FormsApp1.FORMDataSet();
+            this.tableTableAdapter = new FormsApp1.FORMDataSetTableAdapters.TableTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fORMDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAjout
+            // 
+            this.btnAjout.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAjout.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjout.Location = new System.Drawing.Point(35, 331);
+            this.btnAjout.Name = "btnAjout";
+            this.btnAjout.Size = new System.Drawing.Size(111, 71);
+            this.btnAjout.TabIndex = 0;
+            this.btnAjout.Text = "Ajouter";
+            this.btnAjout.UseVisualStyleBackColor = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(159, 331);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(131, 71);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Selectionner";
+            this.btnSelect.UseVisualStyleBackColor = false;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnModifier.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModifier.Location = new System.Drawing.Point(296, 331);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(111, 71);
+            this.btnModifier.TabIndex = 2;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = false;
+            // 
+            // btnSupp
+            // 
+            this.btnSupp.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSupp.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupp.Location = new System.Drawing.Point(414, 331);
+            this.btnSupp.Name = "btnSupp";
+            this.btnSupp.Size = new System.Drawing.Size(111, 71);
+            this.btnSupp.TabIndex = 3;
+            this.btnSupp.Text = "Supprimer";
+            this.btnSupp.UseVisualStyleBackColor = false;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnAnnuler.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnnuler.Location = new System.Drawing.Point(531, 331);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(111, 71);
+            this.btnAnnuler.TabIndex = 4;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            // 
+            // ID
+            // 
+            this.ID.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(214, 175);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(180, 31);
+            this.ID.TabIndex = 5;
+            // 
+            // Nom
+            // 
+            this.Nom.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nom.Location = new System.Drawing.Point(214, 247);
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(180, 31);
+            this.Nom.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ajouter Id :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ajouter nom :";
+            // 
+            // comboBox
+            // 
+            this.comboBox.DataSource = this.tableBindingSource;
+            this.comboBox.DisplayMember = "Id_livre";
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(35, 78);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(606, 28);
+            this.comboBox.TabIndex = 9;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.fORMDataSet;
+            // 
+            // fORMDataSet
+            // 
+            this.fORMDataSet.DataSetName = "FORMDataSet";
+            this.fORMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(725, 450);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Nom);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnSupp);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnAjout);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion des livres";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fORMDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAjout;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupp;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox Nom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox;
+        private FORMDataSet fORMDataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private FORMDataSetTableAdapters.TableTableAdapter tableTableAdapter;
+    }
+}
+
---
+++ FormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        static string chaine = @"Data Source=FATIMA\SQLEXPRESS;Initial Catalog=FORM;Integrated Security=True";
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void afficher()
+        {
+            comboBox1.Items.Clear();
+            cmd.Connection = cnx;
+            cmd.CommandText = "select * from FORM";
+            if (cmd.Connection.State == ConnectionState.Open)
+            {
+                cmd.Connection.Close();
+            }
+            cnx.Open();
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                comboBox1.Items.Add("Name : " + dr["name"]);
+                comboBox1.Items.Add("Id   : " + dr["id"].ToString());
+
+            }
+
+        }
+
+        private void Initialisation()
+        {
+
+            btnAjout.Enabled = true;
+            btnSelect.Enabled = false;
+            btnModifier.Enabled = false;
+            btnSupp.Enabled = true;
+            btnAnnuler.Enabled = true;
+            comboBox.Enabled = true;
+            ID.Enabled = false;
+            Nom.Enabled = false;
+
+        }
+        private void ajout()
+        {
+            this.Text = "L'ajout";
+            ID.Clear();
+            Nom.Clear();
+            afficher();
+            btnAjout.Enabled = false;
+            btnSelect.Enabled = true;
+            btnModifier.Enabled = false;
+            btnSupp.Enabled = true;
+            btnAnnuler.Enabled = false;
+            comboBox.Enabled = false;
+            ID.Enabled = true;
+            Nom.Enabled = true;
+        }
+
+        private void Selectionner()
+        {
+            if (comboBox.SelectedItem == null)
+            {
+                this.Text = "La case est deja vide";
+            }
+            else
+            {
+                this.Text = "Selectionner une case";
+            }
+
+            this.index = comboBox.SelectedIndex;
+
+            btnAjout.Enabled = true;
+            btnSelect.Enabled = false;
+            btnModifier.Enabled = true;
+            btnSupp.Enabled = true;
+            btnAnnuler.Enabled = false;
+            comboBox.Enabled = true;
+            ID.Enabled = false;
+            Nom.Enabled = false;
+
+
+        }
+
+        private void modifier()
+        {
+
+            this.Text = "Modifier";
+            btnAjout.Enabled = false;
+            btnSelect.Enabled = true;
+            btnModifier.Enabled = false;
+            btnSupp.Enabled = false;
+            btnAnnuler.Enabled = true;
+            comboBox.Enabled = true;
+            ID.Enabled = true;
+            Nom.Enabled = true;
+
+        }
+        private void Supprimer()
+        {
+            this.Text = "Supprimer";
+            comboBox.Enabled = false;
+            ID.Enabled = false;
+            Nom.Enabled = false;
+            btnAnnuler.Enabled = true;
+            btnSelect.Enabled = true;
+            btnSupp.Enabled = false;
+            btnAjout.Enabled = false;
+            btnModifier.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialisation();
+        }
+
+
+        private void btnAjout_Click(object sender, EventArgs e)
+        {
+            ajout();
+
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            modifier();
+            afficher();
+        }
+
+        private void btnSupp_Click(object sender, EventArgs e)
+        {
+            string msg = "confirmer la suppression";
+
+            string title = "confiramtion";
+
+
+
+            if (MessageBox.Show(msg, title, MessageBoxButtons.YesNo) == DialogResult.Yes)
+
+            {
+
+                if (cmd.Connection.State == ConnectionState.Open)
+                {
+                    cmd.Connection.Close();
+                }
+                cnx.Open();
+                cmd.Connection = cnx;
+                cmd.CommandText = "delete from id_livre where id='" + ID.Text + "' ";
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+                ID.Clear();
+                Nom.Clear();
+
+                Initialisation();
+            }
+            else
+            {
+                Selectionner();
+            }
+
+            afficher();
+            Supprimer();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            cmd.Connection = cnx;
+            cnx.Open();
+            if (this.Text.Equals("L'ajout"))
+            {
+                cmd.CommandText = "INSERT INTO name_id (id ,nom ) values('" + ID.Text + "','" + Nom.Text + "')";
+                cmd.ExecuteNonQuery();
+            }
+            else if (this.Text.Equals("Modifier"))
+            {
+                cmd.CommandText = "update Livre set name ='" + Nom.Text + "' where id='" + ID.Text + "' ";
+                cmd.ExecuteNonQuery();
+            }
+            cnx.Close();
+        }
+
+
+        private void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            if (this.Text.Equals("L'ajout"))
+            {
+                Initialisation();
+            }
+            else if (this.Text.Equals("Modifier") || this.Text.Equals("Supprimer"))
+            {
+                Selectionner();
+
+            }
+        }
+    }
+}
